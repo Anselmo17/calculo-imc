4f84c052678c5f42092a2ef2204e36d5b777bf6e
Anselmo <ansinftec17@gmail.com>
2021-01-11 21:44:25 -0300

    Adicionando metodo para calculo de massa corporal

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -7,29 +7,50 @@ export class CalculadoraComponent implements OnInit {
   peso : number;
   altura : number;
   idade : string;
+  imcCorporal : any;
+  mensagemUsuario : string;
 
   constructor() {}
 
   ngOnInit() {}
 
 
-  findImc() {
-
-    const alt = Number(this.altura);
+  encontraImc() {
+    let alturaFormatada = this.altura.toString().replace(/,/g, ".");
+    const alt = Number(alturaFormatada);
     const pesoPessoa = Number(this.peso);
 
-    const imc = pesoPessoa / (alt * alt);
-    console.log('===========', imc);
-    return imc;
+    this.imcCorporal = (pesoPessoa / (alt * alt)).toPrecision(4);
+    const imc = Number(this.imcCorporal);
+    this.mensagemUsuario = this.mensagemImcCorporal(imc);
   }
 
   validaCampo(event) {
-  let field = event.target.value.replace(",","");
-   const formatField = field.length > 1 ? `${field[0]},`+field.substring(1 , field.length) : field;
-    console.log(event);
-    const name = event.target.name; 
+    let field = event.target.value.replace(/,/g, "");
+    const formatField = field.length > 1 ? `${
+      field[0]
+    },` + field.substring(1, field.length) : field;
+
+    const name = event.target.name;
     this[name] = formatField;
     return formatField;
+  }
+
+  mensagemImcCorporal(imc) {
+
+    let resultado;
+    switch (imc) {
+      case imc > 18.5: resultado = 'magreza';
+        break;
+      case imc >= 25 || imc <= 29.9: resultado = 'sobrepeso';
+        break;
+      case imc >= 30 || imc <= 39.9: resultado = 'Obesidade';
+        break;
+      case imc > 40: resultado = 'Obesidade';
+        break;
+      default: resultado = 'normal';
+    }
+    return resultado;
   }
 
 
