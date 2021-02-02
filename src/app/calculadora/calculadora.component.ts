1c84961ff138a1851d4c278bdc4c37296f488d7b
Anselmo <ansinftec17@gmail.com>
2021-02-02 20:51:51 -0300

    Ajustando entradas de dados

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,7 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {ImplicitReceiver} from '@angular/compiler';
 
-@Component({selector: 'app-calculadora', templateUrl: './calculadora.component.html', styleUrls: ['./calculadora.component.css']})
+@Component({
+  selector: 'app-calculadora', 
+  templateUrl: './calculadora.component.html', 
+  styleUrls: ['./calculadora.component.css'
+]})
 export class CalculadoraComponent implements OnInit {
 
   peso : number;
@@ -9,6 +13,8 @@ export class CalculadoraComponent implements OnInit {
   idade : string;
   imcCorporal : any;
   mensagemUsuario : string;
+  resultImc : boolean = false;
+
 
   constructor() {}
 
@@ -18,15 +24,16 @@ export class CalculadoraComponent implements OnInit {
   encontraImc() {
     let alturaFormatada = this.altura.toString().replace(/,/g, ".");
     const alt = Number(alturaFormatada);
-    const pesoPessoa = Number(this.peso);
+    const pesoPessoa = Number(this.peso.toString().substring(0, 2));
 
     this.imcCorporal = (pesoPessoa / (alt * alt)).toPrecision(4);
     const imc = Number(this.imcCorporal);
     this.mensagemUsuario = this.mensagemImcCorporal(imc);
+    this.resultImc = true;
   }
 
   validaCampo(event) {
-    let field = event.target.value.replace(/,/g, "");
+    let field = event.target.value.replace(/,/g, "").replace(/\D/g, "");
     const formatField = field.length > 1 ? `${
       field[0]
     },` + field.substring(1, field.length) : field;
@@ -36,21 +43,33 @@ export class CalculadoraComponent implements OnInit {
     return formatField;
   }
 
+  validaPeso(event) {
+    let field = event.target.value.replace(/\D/g, "");
+    const formatField = field.length > 2 ? `${
+      field[0] + field[1]
+    },` + field.substring(2, field.length) : field;
+
+    const name = event.target.name;
+    this[name] = formatField;
+    return formatField;
+  }
+
   mensagemImcCorporal(imc) {
 
     let resultado;
+
+    if (imc < 18.5) 
+      resultado = 'Magreza';
+     else if (imc >= 25 && imc <= 29.9) 
+      resultado = 'Sobrepeso';
+     else if (imc >= 30 && imc <= 39.9) 
+      resultado = 'Obesidade';
+     else if (imc > 40) 
+      resultado = 'Obesidade Grave';
+     else 
+      resultado = 'Normal';
     
-      if(imc < 18.5)
-        resultado = 'magreza';
-      else if(imc >= 25 && imc <= 29.9)
-        resultado = 'sobrepeso';
-      else if(imc >= 30 && imc <= 39.9)
-        resultado = 'Obesidade';
-      else if(imc > 40)
-        resultado = 'Obesidade Grave';
-      else 
-        resultado = 'normal';
-    
+
     return resultado;
   }
 
