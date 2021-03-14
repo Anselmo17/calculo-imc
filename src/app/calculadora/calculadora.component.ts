7ab627131f0dc94ce7077e123ba8cbd06d2c6d17
Anselmo <ansinftec17@gmail.com>
2021-03-14 12:48:14 -0300

    Corrigindo validaçoes dos dados de entrada

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -4,12 +4,12 @@ import {ImplicitReceiver} from '@angular/compiler';
 @Component({selector: 'app-calculadora', templateUrl: './calculadora.component.html', styleUrls: ['./calculadora.component.css']})
 export class CalculadoraComponent implements OnInit {
 
-  peso : number;
-  altura : number;
-  idade : string;
-  imcCorporal : any;
-  mensagemUsuario : string;
-  resultImc : boolean = false;
+  peso: number;
+  altura: number;
+  idade: string;
+  imcCorporal: any;
+  mensagemUsuario: string;
+  resultImc = false;
 
 
   constructor() {}
@@ -18,7 +18,7 @@ export class CalculadoraComponent implements OnInit {
 
 
   encontraImc() {
-    let alturaFormatada = this.altura.toString().replace(/,/g, ".");
+    const alturaFormatada = this.altura.toString().replace(/,/g, '.');
     const alt = Number(alturaFormatada);
     const pesoPessoa = Number(this.peso.toString().substring(0, 2));
 
@@ -29,7 +29,7 @@ export class CalculadoraComponent implements OnInit {
   }
 
   validaCampo(event) {
-    let field = event.target.value.replace(/,/g, "").replace(/\D/g, "");
+    const field = event.target.value.replace(/,/g, '').replace(/\D/g, '');
     const formatField = field.length > 1 ? `${
       field[0]
     },` + field.substring(1, field.length) : field;
@@ -40,7 +40,7 @@ export class CalculadoraComponent implements OnInit {
   }
 
   validaPeso(event) {
-    let field = event.target.value.replace(/\D/g, "");
+    const field = event.target.value.replace(/\D/g, '');
     const formatField = field.length > 2 ? `${
       field[0] + field[1]
     },` + field.substring(2, field.length) : field;
@@ -54,17 +54,17 @@ export class CalculadoraComponent implements OnInit {
 
     let resultado;
 
-    if (imc < 18.5) 
+    if (imc < 18.5) {
       resultado = 'Magreza';
-     else if (imc >= 25 && imc <= 29.9) 
+    } else if (imc >= 25 && imc <= 29.9) {
       resultado = 'Sobrepeso';
-     else if (imc >= 30 && imc <= 39.9) 
+    } else if (imc >= 30 && imc <= 39.9) {
       resultado = 'Obesidade';
-     else if (imc > 40) 
+    } else if (imc > 40) {
       resultado = 'Obesidade Grave';
-     else 
+    } else {
       resultado = 'Normal';
-    
+    }
     return resultado;
   }
 
